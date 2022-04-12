5774e4d6c66983a5907bedc08d6ac9bc7932e5f3
unknown <davidichunlin@gmail.com>
2022-04-12 12:08:08 +0800

    update

--- wd/wd.js
+++ wd/wd.js
@@ -15,11 +15,22 @@ function slideShowControl() {
   if (autoPlay == true) {
     autoPlay = false;
     document.getElementById("slideCanvas").style.border = "3px ridge gold";
+    document.getElementById("viewPrev").style.visibility = "visible";
+    document.getElementById("viewNext").style.visibility = "visible";
     clearInterval(myInterval);
   }
   else {
     autoPlay = true;
     document.getElementById("slideCanvas").style.border = "3px solid darkgray";
+    document.getElementById("viewPrev").style.visibility = "hidden";
+    document.getElementById("viewNext").style.visibility = "hidden";
     myInterval = setInterval(autoPlayOn, 4000);
   }
+}
+
+function flip(x) {
+  currentView += x;
+  if (currentView > 8) {currentView = 1};
+  if (currentView < 1) {currentView = 8};
+  document.getElementById("slideCanvas").style.backgroundImage = "url('../SampleAlbum/BW/" + currentView + ".jpg')";
 }
