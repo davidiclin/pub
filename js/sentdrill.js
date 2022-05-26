39295127774bb7ca0167e1b83e57dc8180f973c1
unknown <davidichunlin@gmail.com>
2022-05-26 11:16:35 +0800

    update

--- js/sentdrill.js
+++ js/sentdrill.js
@@ -3,6 +3,6 @@ getText.open("GET","https://davidiclin.github.io/pub/json/sentdrill.json",false)
 getText.send(null);
 const contentAll = JSON.parse(getText.responseText);
 
-document.getElementById("questionBody").innerHTML = contentAll[0].id + contentAll[0].qBody;
+document.getElementById("questionBody").innerHTML = contentAll[0].id + ". " + contentAll[0].qBody;
 document.getElementById("answer").value = contentAll[0].hint;
 document.getElementById("key").innerHTML = contentAll[0].key;
