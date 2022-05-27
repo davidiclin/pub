da54d67df0ddd8e4202a79212e10cdbdbaae4822
unknown <davidichunlin@gmail.com>
2022-05-27 11:53:48 +0800

    update

--- js/sentdrill.js
+++ js/sentdrill.js
@@ -4,5 +4,22 @@ getText.send(null);
 const contentAll = JSON.parse(getText.responseText);
 
 document.getElementById("questionBody").innerHTML = contentAll[1].id + ". " + contentAll[1].qBody;
-document.getElementById("answer").value = contentAll[1].hint;
+document.getElementById("answer").innerHTML = contentAll[1].hint;
+document.getElementById("blocks").innerHTML = getBlocks();
 document.getElementById("key").innerHTML = contentAll[1].key;
+
+function getBlocks() {
+    blocks = contentAll[1].qBody.replace(contentAll[1].hint, "").strip(".").split(" ");
+    for (var count = 0; count < blocks.length; count++) {  // Shuffling
+      var n1 = Math.floor(Math.random() * blocks.length);
+      var n2 = Math.floor(Math.random() * blocks.length);
+      tmp = blocks[n1];
+      blocks[n1] = blocks[n2];
+      blocks[n2] = tmp;
+    }
+    var blocksHTML = "";
+    for (var count = 0; count < blocks.length; count++) {
+      blocksHTML += "<span>" + blocks[count] + "</span> ";
+    }
+    return blocksHTML;
+}
