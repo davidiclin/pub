4fdcbabe56dcf9932bed8e28d5e0415fe1acce76
unknown <davidichunlin@gmail.com>
2022-07-20 10:41:39 +0800

    update

--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,10 @@
 var plan = [5,6,1,3,2,7,4,8,9,9,8,4,1,5,6,3,7,2,3,7,2,4,8,9,6,1,5,1,4,9,6,3,5,8,2,7,7,3,8,2,4,1,5,9,6,2,5,6,7,9,8,1,4,3,6,9,5,8,1,2,7,3,4,4,1,7,9,6,3,2,5,8,8,2,3,5,7,4,9,6,1]
 var puzzle = [0,0,0,3,0,0,0,0,9,0,0,0,0,0,0,0,7,0,3,0,2,0,8,9,0,1,5,0,4,0,0,0,5,0,2,0,0,0,8,0,0,1,0,9,6,0,0,0,7,0,8,0,0,0,6,0,0,0,1,0,0,3,4,0,0,7,0,6,3,0,0,8,0,2,0,5,7,0,0,6,0]
 
-for (count1 = 0; count1 < 9; count1++) {
-  for (count2 = 0; count2 < 9; count2++) {
+shufflePuzzle()
+
+for (var count1 = 0; count1 < 9; count1++) {
+  for (var count2 = 0; count2 < 9; count2++) {
     if (puzzle[count1 * 9 + count2] == 0) {
       document.getElementById("matrix").innerHTML += "<form class=\"cellContainer\" id=\"f" + (count1 * 9 + count2) + "\"><input class=\"cell\" type=\"number\" min=\"1\" max=\"9\" name=\"f" + (count1 * 9 + count2) + "\"></form>"
     }
@@ -12,28 +14,133 @@ for (count1 = 0; count1 < 9; count1++) {
   }
   document.getElementById("matrix").innerHTML += "<br>"
 }
-for (count = 0; count < 81; count++) {
+for (var count = 0; count < 81; count++) {
   thisId = "f" + count
   if (count < 9) {
-    document.getElementById(thisId).style.borderTop = "3px solid black";
+    document.getElementById(thisId).style.borderTop = "3px outset MediumAquaMarine";
   }
   if (count > 26 & count < 36) {
-    document.getElementById(thisId).style.borderTop = "3px solid black";
+    document.getElementById(thisId).style.borderTop = "3px outset MediumAquaMarine";
   }
   if (count > 53 & count < 63) {
-    document.getElementById(thisId).style.borderTop = "3px solid black";
+    document.getElementById(thisId).style.borderTop = "3px outset MediumAquaMarine";
   }
   if (count > 71) {
-    document.getElementById(thisId).style.borderBottom = "3px solid black";
+    document.getElementById(thisId).style.borderBottom = "3px outset MediumAquaMarine";
   }
   if (count % 3 == 0) {
-    document.getElementById(thisId).style.borderLeft = "3px solid black";
+    document.getElementById(thisId).style.borderLeft = "3px outset MediumAquaMarine";
   }
   if (count % 9 == 8) {
-    thisId = "f" + count
-    document.getElementById(thisId).style.borderRight = "3px solid black";
+    document.getElementById(thisId).style.borderRight = "3px outset MediumAquaMarine";
   }
   if (puzzle[count] == 0) {
-    document.getElementById(thisId).style.backgroundColor = "rgba(155, 155, 175, .35)";
+    document.getElementById(thisId).style.backgroundColor = "rgba(72, 209, 204, .25)";
+  }
+}
+
+function shufflePuzzle() {
+  var rotateCount = Math.floor(Math.random() * 3);
+  for (var count = 0; count < rotateCount; count++) {
+    rotateIt()
+  }
+
+  var swapTokenCount = Math.floor(Math.random() * 3);
+  for (var count = 0; count < swapTokenCount; count++) {
+    swapToken()
+  }
+
+  swapBigRows();
+
+  var swapTokenCount = Math.floor(Math.random() * 3);
+  for (var count = 0; count < swapTokenCount; count++) {
+    swapRows()
+  }
+}
+
+function rotateIt() {
+  var newPl = [];
+  var newPz = [];
+  for (var count1 = 72; count1 < 81; count1++) {
+    for (var count2 = 0; count2 < 9; count2++) {
+      newPl.push(plan[count1 - count2 * 9]);
+      newPz.push(puzzle[count1 - count2 * 9]);
+    }
+  }
+  plan = newPl;
+  puzzle = newPz;
+}
+
+function swapToken() {
+  var swap1 = Math.floor(Math.random() * 9);
+  var swap2 = Math.floor(Math.random() * 9);
+  if (swap1 == swap2) {
+    return
+  }
+  for (var count = 0; count < 81; count ++) {
+    if (plan[count] == swap1) {
+      plan[count] = 10;
+      if (puzzle[count] != 0) {
+        puzzle[count] = 10;
+      }
+    }
+  }
+  for (var count = 0; count < 81; count ++) {
+    if (plan[count] == swap2) {
+      plan[count] = swap1;
+      if (puzzle[count] != 0) {
+        puzzle[count] = swap1;
+      }
+    }
+  }
+  for (var count = 0; count < 81; count ++) {
+    if (plan[count] == 10) {
+      plan[count] = swap2;
+      if (puzzle[count] == 10) {
+        puzzle[count] = swap2;
+      }
+    }
+  }
+}
+
+function swapBigRows() {
+  var newPl = []
+  var newPz = []
+  var swap1 = Math.floor(Math.random() * 3);
+  var swap2 = Math.floor(Math.random() * 3);
+  if (swap1 == swap2) {
+    return
+  }
+  var swap3 = [0,1,2].filter(x => x != swap1 & x != swap2)[0]
+  for (var count = 0; count < 27; count++) {
+    newPl.push(plan[count + swap1 * 27])
+    newPz.push(puzzle[count + swap1 * 27])
+  }
+  for (var count = 0; count < 27; count++) {
+    newPl.push(plan[count + swap2 * 27])
+    newPz.push(puzzle[count + swap2 * 27])
+  }
+  for (var count = 0; count < 27; count++) {
+    newPl.push(plan[count + swap3 * 27])
+    newPz.push(puzzle[count + swap3 * 27])
+  }
+  plan = newPl;
+  puzzle = newPz
+}
+
+function swapRows() {
+  var seed = Math.floor(Math.random() * 3);
+  var swap1 = Math.floor(Math.random() * 3) + seed * 3;
+  var swap2 = Math.floor(Math.random() * 3) + seed * 3;
+  if (swap1 == swap2) {
+    return
+  }
+  for (count = 0; count < 9; count++) {
+    var tempPl = plan[swap1 * 9 + count];
+    var tempPz = puzzle[swap1 * 9 + count];
+    plan[swap1 * 9 + count] = plan[swap2 * 9 + count];
+    puzzle[swap1 * 9 + count] = puzzle[swap2 * 9 + count];
+    plan[swap2 * 9 + count] = tempPl;
+    puzzle[swap2 * 9 + count] = tempPz
   }
 }
